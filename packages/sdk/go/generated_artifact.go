dd6408eb26c9a5cae339dec849f6fb264c4413a9
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2026-09-14 13:55:16 +0900

    chore: pin dependencies

--- packages/sdk/go/generated_artifact.go
+++ packages/sdk/go/generated_artifact.go
@@ -1,6 +1,6 @@
 // Code generated for this Wasm build. DO NOT EDIT.
 package markdown
 
-const buildID = "015d42a01b73e510"
+const buildID = "b0586cbfb94ac015"
 const inputBytes = 1048576
 const memoryPages = 512
--- packages/sdk/typescript/generated/artifact.ts
+++ packages/sdk/typescript/generated/artifact.ts
@@ -1,3 +1,3 @@
 // Generated for this Wasm build. Do not edit.
-export const buildId = '015d42a01b73e510'
+export const buildId = 'b0586cbfb94ac015'
 export const inputBytes = 1048576
