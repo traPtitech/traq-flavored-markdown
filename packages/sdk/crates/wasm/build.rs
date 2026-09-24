4d0688cb4aa4aaae5952f99330ce800128403aa1
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2026-09-25 01:16:29 +0900

    build: hash only crate inputs for wasm build id

--- packages/sdk/crates/wasm/build.rs
+++ packages/sdk/crates/wasm/build.rs
@@ -26,15 +26,32 @@ fn input(path: &Path, hash: &mut DefaultHasher) {
     }
 }
 
+fn crate_sources(root: &Path, hash: &mut DefaultHasher) {
+    println!("cargo:rerun-if-changed={}", root.display());
+    let mut crates: Vec<_> = fs::read_dir(root)
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+        .filter(|path| path.join("Cargo.toml").is_file())
+        .collect();
+    crates.sort();
+    for path in crates {
+        input(&path.join("Cargo.toml"), hash);
+        input(&path.join("src"), hash);
+        let build_script = path.join("build.rs");
+        if build_script.is_file() {
+            input(&build_script, hash);
+        }
+    }
+}
+
 fn main() {
     let mut hash = DefaultHasher::new();
     for path in [
         "src",
-        "../grammar",
-        "../processing",
-        "../../../core/crates",
-        "../../../plugins/commonmark/crates",
-        "../../../plugins/traq/crates",
+        "../grammar/src",
+        "../grammar/Cargo.toml",
+        "../processing/src",
+        "../processing/Cargo.toml",
         "build.rs",
         "Cargo.toml",
         "../../../../Cargo.toml",
@@ -43,6 +60,13 @@ fn main() {
         "../../../../.cargo/config.toml",
     ] {
         input(Path::new(path), &mut hash);
+    }
+    for root in [
+        "../../../core/crates",
+        "../../../plugins/commonmark/crates",
+        "../../../plugins/traq/crates",
+    ] {
+        crate_sources(Path::new(root), &mut hash);
     }
     println!("cargo:rustc-env=MARKDOWN_BUILD_ID={:016x}", hash.finish());
 }
